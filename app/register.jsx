b19dcd1e08567c0124091f017f298e4eb5369192
maldikurniawan <akurniawan2102@gmail.com>
2024-10-17 11:32:42 +0700

    fix bug

--- app/register.jsx
+++ app/register.jsx
@@ -1,5 +1,17 @@
 import React, { useState } from "react";
-import { Text, View, TextInput, Image, SafeAreaView, TouchableOpacity, StatusBar, Alert, ScrollView, KeyboardAvoidingView, Platform } from "react-native";
+import {
+    Text,
+    View,
+    TextInput,
+    Image,
+    SafeAreaView,
+    TouchableOpacity,
+    StatusBar,
+    Alert,
+    ScrollView,
+    KeyboardAvoidingView,
+    Platform,
+} from "react-native";
 import tw from "twrnc";
 import { useNavigation } from "expo-router";
 
@@ -32,53 +44,63 @@ export default function Register() {
                     <Image source={backImage} style={tw`absolute top-0 w-full h-80`} />
                     <View style={tw`absolute bottom-0 w-full h-[700px] bg-white rounded-t-3xl`} />
                     <SafeAreaView style={tw`flex-1 justify-center mx-8`}>
-                        <Text style={tw`text-4xl font-bold text-orange-500 text-center my-8`}>Register</Text>
+                        <Text style={tw`text-4xl font-bold text-orange-500 text-center mb-4 mt-12`}>Register</Text>
 
-                        <TextInput
-                            style={tw`bg-gray-200 h-14 mb-5 text-lg rounded-xl px-4`}
-                            placeholder="Enter username"
-                            autoCapitalize="none"
-                            value={username}
-                            onChangeText={(text) => setUsername(text)}
-                        />
+                        <View>
+                            <Text style={tw`text-gray-700 font-semibold mb-1`}>Username</Text>
+                            <TextInput
+                                style={tw`bg-gray-200 text-lg rounded-xl p-2 mb-4`}
+                                autoCapitalize="none"
+                                value={username}
+                                onChangeText={(text) => setUsername(text)}
+                            />
+                        </View>
 
-                        <TextInput
-                            style={tw`bg-gray-200 h-14 mb-5 text-lg rounded-xl px-4`}
-                            placeholder="Enter firstname"
-                            autoCapitalize="words"
-                            value={firstname}
-                            onChangeText={(text) => setFirstname(text)}
-                        />
+                        <View>
+                            <Text style={tw`text-gray-700 font-semibold mb-1`}>Firstname</Text>
+                            <TextInput
+                                style={tw`bg-gray-200 text-lg rounded-xl p-2 mb-4`}
+                                autoCapitalize="words"
+                                value={firstname}
+                                onChangeText={(text) => setFirstname(text)}
+                            />
+                        </View>
 
-                        <TextInput
-                            style={tw`bg-gray-200 h-14 mb-5 text-lg rounded-xl px-4`}
-                            placeholder="Enter lastname"
-                            autoCapitalize="words"
-                            value={lastname}
-                            onChangeText={(text) => setLastname(text)}
-                        />
+                        <View>
+                            <Text style={tw`text-gray-700 font-semibold mb-1`}>Lastname</Text>
+                            <TextInput
+                                style={tw`bg-gray-200 text-lg rounded-xl p-2 mb-4`}
+                                autoCapitalize="words"
+                                value={lastname}
+                                onChangeText={(text) => setLastname(text)}
+                            />
+                        </View>
 
-                        <TextInput
-                            style={tw`bg-gray-200 h-14 mb-5 text-lg rounded-xl px-4`}
-                            placeholder="Enter password"
-                            autoCapitalize="none"
-                            autoCorrect={false}
-                            secureTextEntry={true}
-                            value={password}
-                            onChangeText={(text) => setPassword(text)}
-                        />
+                        <View>
+                            <Text style={tw`text-gray-700 font-semibold mb-1`}>Password</Text>
+                            <TextInput
+                                style={tw`bg-gray-200 text-lg rounded-xl p-2 mb-4`}
+                                autoCapitalize="none"
+                                autoCorrect={false}
+                                secureTextEntry={true}
+                                value={password}
+                                onChangeText={(text) => setPassword(text)}
+                            />
+                        </View>
 
-                        <TextInput
-                            style={tw`bg-gray-200 h-14 mb-5 text-lg rounded-xl px-4`}
-                            placeholder="Retype password"
-                            autoCapitalize="none"
-                            autoCorrect={false}
-                            secureTextEntry={true}
-                            value={retypePassword}
-                            onChangeText={(text) => setRetypePassword(text)}
-                        />
+                        <View>
+                            <Text style={tw`text-gray-700 font-semibold mb-1`}>Retype Password</Text>
+                            <TextInput
+                                style={tw`bg-gray-200 text-lg rounded-xl p-2 mb-4`}
+                                autoCapitalize="none"
+                                autoCorrect={false}
+                                secureTextEntry={true}
+                                value={retypePassword}
+                                onChangeText={(text) => setRetypePassword(text)}
+                            />
+                        </View>
 
-                        <TouchableOpacity style={tw`bg-orange-500 h-14 rounded-xl justify-center items-center mt-10`} onPress={onHandleRegister}>
+                        <TouchableOpacity style={tw`bg-orange-500 h-14 rounded-xl justify-center items-center mt-2`} onPress={onHandleRegister}>
                             <Text style={tw`text-white font-bold text-lg`}>Register</Text>
                         </TouchableOpacity>
 
